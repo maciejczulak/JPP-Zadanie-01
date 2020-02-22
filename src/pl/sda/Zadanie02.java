4f055b096ed6fa377ed6ea0aefa7fd6c5b30ee30
Maciej Czulak <maciejczulak@o2.pl>
2020-02-22 15:45:10 +0100

    change smth in project

--- src/pl/sda/Zadanie02.java
+++ src/pl/sda/Zadanie02.java
@@ -1,7 +1,15 @@
 package pl.sda;
-
+import java.util.Scanner;
 public class Zadanie02 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Name: ");
+        String name = sc.nextLine();
+        System.out.println("Surname: ");
+        String surname = sc.nextLine();
+        System.out.println("Age: ");
+        Short age = sc.nextShort();
+
+
     }
 }
